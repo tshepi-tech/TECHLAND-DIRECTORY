b3e18f2beb775171107f4a9611c51dd76423e86d
tshepiso <tshepi.tech@gmail.com>
2023-12-10 14:14:06 +0100

    cake companies listed

--- src/CompanyItem.jsx
+++ src/CompanyItem.jsx
@@ -1,11 +1,14 @@
 export default function CompanyItem({ company }) {
-	const { id, name, city, quote } = company;
+	const { name, city, quote } = company;
 	return (
-		<ul>
-			<li>{id}</li>
-			<li>{name}</li>
-			<li>{city}</li>
-			<li>{quote}</li>
-		</ul>
+		<div>
+			<div>
+				<h3>{name}</h3>
+				<p>{city}</p>
+				<p>{quote}</p>
+				<button popovertarget="details">more</button>
+			</div>
+			<div></div>
+		</div>
 	);
 }
