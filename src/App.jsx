ab53e8db98395a2d5e139d5ca684f913b52c4745
tshepiso <tshepi.tech@gmail.com>
2023-12-13 22:50:01 +0100

    css for company card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import companies from "./Data/companies.json";
 import CompaniesList from "./CompaniesList";
+import "./App.css";
 
 export default function App() {
 	return <CompaniesList companies={companies} />;
--- src/CompanyItem.jsx
+++ src/CompanyItem.jsx
@@ -1,14 +1,20 @@
+import image from "./Assets/starmap.png";
+
 export default function CompanyItem({ company }) {
 	const { name, city, quote } = company;
 	return (
-		<div>
-			<div>
-				<h3>{name}</h3>
-				<p>{city}</p>
-				<p>{quote}</p>
-				<button popovertarget="details">more</button>
+		<div className="company_card">
+			<div className="image_title">
+				<img src={image}></img>
+				<div className="title">
+					<p className="name">{name}</p>
+					<p className="city">{city}</p>
+				</div>
 			</div>
-			<div></div>
+			<div className="quote_contact">
+				<p className="quote">{quote}</p>
+				<button>Contact</button>
+			</div>
 		</div>
 	);
 }
