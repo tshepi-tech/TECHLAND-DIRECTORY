cdf9af5ce0ca1ef462710d5d3aad323cb33aa1bd
tshepiso <tshepi.tech@gmail.com>
2023-12-13 23:15:41 +0100

    structure of website prepared

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,11 @@ import CompaniesList from "./CompaniesList";
 import "./App.css";
 
 export default function App() {
-	return <CompaniesList companies={companies} />;
+	return (
+		<div>
+			<h1>GotCake?</h1>
+			<div className="filter_group">All malmo lund other</div>
+			<CompaniesList companies={companies} />;
+		</div>
+	);
 }
